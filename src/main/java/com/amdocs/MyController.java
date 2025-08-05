65dd34834cef5d435470a15b65b81a9592d09db9
unknown <namanjn619@gmail.com>
2025-08-05 12:15:33 +0530

    first commit

---
+++ src/main/java/com/amdocs/MyController.java
@@ -0,0 +1,11 @@
+package com.amdocs;
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @GetMapping("/")
+    public String Home(){
+        return "Welcome to Jenkins Contoller";
+    }
+}
---
+++ src/test/java/com/amdocs/Democicd2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Democicd2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
